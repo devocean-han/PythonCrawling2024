d60a1aff26264383b705dab01b2a1fc6472d2a45
DevOcean Han <soa6318@gmail.com>
2024-03-06 23:47:22 +0900

    [추가] 사용자 추가 이미지용 모듈 정의
    [images.py]
     - 각 사이트를 대상으로 스크래핑 진행시 이곳에서 고정 이미지 url을 참조함
     - 사용자 측에서 각 사이트별 고정 이미지 url을 추가할 수 있도록 함이 목적
     - 최초 배포 이후 배포에서는 덮어씌우면 안됨

---
+++ images.py
@@ -0,0 +1,64 @@
+
+IMAGES = {
+	"샘플": { 
+		"상": ["https://vans_상단_이미지.jpg", "https://vans_상품이미지보다_위.png"],
+		"중": ["https://vans_가운데_이미지.jpg", "https://vans_설명문구보다_위.jpeg"],
+		"하": ["https://vans_하단_이미지.jpg", "https://vans_사이즈표.webp", "https://배송_안내_이미지.jpg"],
+	},
+	"default": {
+		"상": ["https://m.media-amazon.com/images/G/01/Zappos/Ralph-Lauren-May-2023/Ralph-Lauren-Mens-MGrid.jpg"],
+		"중": ["https://www.zappos.com/boutiques/3130/poloralphlauren_header110122.gif"],
+		"하": ["https://shop-phinf.pstatic.net/20231228_296/1703718751132RIyXT_JPEG/sizechartlogo.jpg?type=w860"],
+	},
+	"Vans": {
+		"상": [],
+		"중": [],
+		"하": [],
+	},
+	"Fila": {
+		"상": [],
+		"중": [],
+		"하": [],
+	},
+	"Lacoste": {
+		"상": [],
+		"중": [],
+		"하": [],
+	},
+	"Crocs": {
+		"상": [],
+		"중": [],
+		"하": [],
+	},
+	"North Face": {
+		"상": [],
+		"중": [],
+		"하": [],
+	},
+	"Columbia": {
+		"상": [],
+		"중": [],
+		"하": [],
+	},
+	"Salomon": {
+		"상": [],
+		"중": [],
+		"하": [],
+	},
+	"Asics": {
+		"상": [],
+		"중": [],
+		"하": [],
+	},
+	"Hoka": {
+		"상": [],
+		"중": [],
+		"하": [],
+	},
+	"Puma": {
+		"상": [],
+		"중": [],
+		"하": [],
+	},
+	
+}

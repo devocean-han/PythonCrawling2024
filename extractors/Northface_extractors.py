365af7df7eda0fb4fce17052eac106e5b2d91085
DevOcean Han <soa6318@gmail.com>
2024-05-10 11:12:50 +0900

    [삭제] GUI 관련 임시 파일 제거

--- extractors/Northface_extractors.py
+++ extractors/Northface_extractors.py
@@ -7,31 +7,13 @@ North Face(노스페이스) 추출자들을 모아놓은 모듈.
 '''
 
 import json
-from pprint import pprint #TODO: 임시. 지울 것
+from pprint import pprint
 import logging
 from bs4 import BeautifulSoup as bs
 
 from extractors import extractors_interface as I
 from strategy_decorator import export_strategy
 
-logger = logging.getLogger('North_Face.Extractors')
-SITE_OFFICIAL = 'North Face'
+logger = logging.getLogger('NorthFace.Extractors')
+SITE_OFFICIAL = 'NorthFace'
 
-@export_strategy(SITE_OFFICIAL)
-class LacosteProductNameExtractor(I.ProductNameExtractor):
-	def __init__(self) -> None:
-		self.product_name_selectors = {
-			'h1': 'article h1.font-large',
-			'script': 'script[type="application/ld+json"]',
-		}	
-	def get_product_name(self, soup) -> str:
-		try: #TODO: 'script' 선택자로는 안 해봤음
-			if not isinstance(soup, bs): # Soup이 아니라 메타데이터 Dict이 주어진 경우 
-				return soup['product']['name']
-			script_tag = soup.select_one(self.product_name_selectors['script'])
-			metadata = json.loads(script_tag.string)
-			product_name = metadata['itemListElement'][-1]['item']['name']
-			return product_name
-		except Exception as e:
-			logger.error(e)
-			return ''
